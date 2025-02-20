3a064919501425470f022f2563cb451caa6ba411
praveen88p <praveenpatidarpatidar88@gmail.com>
2025-02-21 01:12:24 +0530

    linkedlist

---
+++ Linkedlist/singly.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using  namespace std;
+
+
+class Node{
+
+    public:
+    int data;
+    Node* next;
+
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+
+int main(){
+   
+
+    Node* node1 = new Node(20);
+    cout<<node1->data;
+    cout<<node1->next;
+    return 0;
+}
