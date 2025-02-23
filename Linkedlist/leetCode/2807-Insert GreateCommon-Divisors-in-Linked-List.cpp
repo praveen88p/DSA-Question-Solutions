0debceb7c0d6138d4230e8353f6ed33947e20ea0
praveen88p <praveenpatidarpatidar88@gmail.com>
2025-02-23 21:11:28 +0530

    leetcode-solved

---
+++ Linkedlist/leetCode/2807-Insert GreateCommon-Divisors-in-Linked-List.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    public:
+        ListNode* insertGreatestCommonDivisors(ListNode* head) {
+            
+            if(!head || head->next ==NULL){
+                return head;
+            }
+    
+            ListNode*temp = insertGreatestCommonDivisors(head->next);
+    
+            ListNode* gcdNode = new ListNode(__gcd(head->val,head->next->val));
+            gcdNode->next = temp;
+            head->next = gcdNode;
+            return head;
+        }
+    };
